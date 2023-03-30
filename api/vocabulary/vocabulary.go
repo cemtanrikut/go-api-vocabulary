6a50c9bfd6484cbd111168901f4b030fbcaf2ed2
Cem Tanrikut <cemtanrikut@gmail.com>
2023-03-30 01:42:02 -0700

    Add lib for translate and translate func

--- api/vocabulary/vocabulary.go
+++ api/vocabulary/vocabulary.go
@@ -1,9 +1,26 @@
 package api
 
+import (
+	"fmt"
+
+	gt "github.com/bas24/googletranslatefree"
+)
+
 type VocabularyData struct {
 	ID         string
 	Word       string
 	Meaning    string
 	CreateDate string
 	IsActive   bool
+}
+
+func TranslateText(langFrom, langTo, text string) (string, error) {
+	result, err := gt.Translate(text, langFrom, langTo) //en, tr
+	if err != nil {
+		fmt.Println("Translation error")
+		return "", fmt.Errorf("Translation Error")
+	}
+	fmt.Println(result)
+
+	return result, nil
 }

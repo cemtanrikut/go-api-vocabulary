4878474283de70cc033ea10841f5c899fa851aa7
Cem Tanrikut <cemtanrikut@gmail.com>
2023-03-30 01:53:05 -0700

    Update TranslateText func and Add Save to db func
    DB integration not exist

--- api/vocabulary/vocabulary.go
+++ api/vocabulary/vocabulary.go
@@ -2,25 +2,54 @@ package api
 
 import (
 	"fmt"
+	"time"
 
 	gt "github.com/bas24/googletranslatefree"
 )
 
 type VocabularyData struct {
 	ID         string
-	Word       string
-	Meaning    string
+	Response   Response
 	CreateDate string
 	IsActive   bool
 }
 
-func TranslateText(langFrom, langTo, text string) (string, error) {
+type Response struct {
+	LangFrom string
+	LangTo   string
+	Text     string
+	Result   string
+}
+
+func TranslateText(langFrom, langTo, text string) (Response, error) {
 	result, err := gt.Translate(text, langFrom, langTo) //en, tr
 	if err != nil {
 		fmt.Println("Translation error")
-		return "", fmt.Errorf("Translation Error")
+		return Response{}, fmt.Errorf("Translation Error")
 	}
-	fmt.Println(result)
 
-	return result, nil
+	resp := Response{
+		LangFrom: langFrom,
+		LangTo:   langTo,
+		Text:     text,
+		Result:   result,
+	}
+
+	return resp, nil
+}
+
+func SaveTextToDB(data Response) error {
+	resp := VocabularyData{
+		ID:         "",
+		Response:   data,
+		CreateDate: time.Now().GoString(),
+		IsActive:   true,
+	}
+
+	fmt.Println(resp)
+
+	// TODO: Should save to DB from here
+	// ...
+
+	return nil
 }

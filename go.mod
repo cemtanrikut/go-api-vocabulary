e740295291bc2fa94108a6bf9139bac536552c4f
Cem Tanrikut <cemtanrikut@gmail.com>
2023-04-06 19:42:44 -0700

    add gorilla mux router

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require github.com/bas24/googletranslatefree v0.0.0-20220326200502-05ed9e639439
 
 require (
+	github.com/gorilla/mux v1.8.0
 	github.com/robertkrimen/otto v0.2.1 // indirect
 	golang.org/x/text v0.8.0 // indirect
 	gopkg.in/sourcemap.v1 v1.0.5 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/translate/{text}", TranslateHandler).Methods(http.MethodGet)
+
+	log.Println("Listening ...")
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		log.Fatalln("There's an error with the server,")
+	}
+}
+
+func TranslateHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 }
